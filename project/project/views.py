62ba8a32a3300b384b184917989eec6cd4c1a5be
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2020-12-28 21:03:24 +0200

    Finished the project.

--- project/project/views.py
+++ project/project/views.py
@@ -16,32 +16,28 @@ from myauth.models import MyUser
 # MyUser is ready to use (requires modifications)!
 # Instead I would go check myauth/models.py and project/settings.py
 
-def index(request):
+@login_required    # FLAW 3.0: Must be authenticated 
+def index(request):# FLAW 3.1: Provide only for user that requested
     notes = {}
-    if request.user.is_authenticated:
-        notes = Note.objects.filter(owner_name= request.user.username)
+    notes = Note.objects.filter(owner_name= request.user.username)
     return render(request,'home.html', {'notes' : notes})
 
-@login_required
+@login_required # FLAW 3 (same apply than in comments of index())
 def add_note(request):
-    # FLAW 4: Prevent user to add malicious html or scripts
-    # See that this now limits the functionality
-    # So if this app is going to be a notebook for
-    # html coder where he/she adds keeps different syntax and tags
-    # then there should be some other kind of validation than this.
-    # Perhaps the bleach package. This however works for me!
+    # FLAW 4.0: Don't allow tags and escape for html and scripts
     form_note = escape(strip_tags(request.POST.get('note')))
     Note.objects.create(owner_name= request.user, note= form_note)
     return redirect(index)
 
-@login_required
+@login_required # FLAW 3 (same apply than in comments of index())
 def search_note(request):
     searched_notes = {}
-    if request.user.is_authenticated:
-        # FLAW 1: SOS raw SQL queries!!
-        #(<- remove to test) query = 'SELECT * FROM note WHERE owner_name LIKE "'+ request.user.username +'" and note LIKE "%' + request.POST.get('note') + '%"'
-        #(<- remove to test) searched_notes = Note.objects.raw(query)
-        # SQL-INJECTION (when added to 'Search'): IbetThisWordWontExist'%" UNION SELECT * FROM note WHERE "%" = "
-        # Instead use covered functions no raw SQL-queries
-        searched_notes = Note.objects.filter(owner_name= request.user.username, note__contains= request.POST.get('note'))
+    # FLAW 1: SOS raw SQL queries!!
+    #(<- remove to test) query = 'SELECT * FROM note WHERE owner_name LIKE "'+ request.user.username +'" and note LIKE "%' + request.POST.get('note') + '%"'
+    #(<- remove to test) searched_notes = Note.objects.raw(query)
+    # SQL-INJECTION (when added to 'Search'): 
+    # IbetThisWordWontExist'%" UNION SELECT * FROM note WHERE "%" = "
+    # FLAW 4.1: Check also query parameters! See 4.0.
+    form_note = escape(strip_tags(request.POST.get('note')))
+    searched_notes = Note.objects.filter(owner_name= request.user.username, note__contains= form_note)
     return render(request,'home.html', {'searched_notes' : searched_notes})
